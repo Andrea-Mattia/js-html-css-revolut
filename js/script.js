19a0cae4cb1ddcc0a40bb5a5efb79f4d53f2342a
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-11 23:15:42 +0200

    add solution

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
-console.log('Works!');
-console.log('jQuery', $);
+/**
+ * Descrizione
+ * Riproduciamo con l'ausilio di jQuery il dropdown menu del sito: https://www.revolut.com/en-IT/
+ */
+$(document).ready(function() {
+
+    /**
+     * Dropdown menu
+     */
+
+    var dropDown = $('.whit-dropdown');
+    var dropLink = dropDown.children('a');
+    var dropMenu = dropDown.children('.dropdown-menu');
+ 
+    // Click per mostrare e nascondere il sottomenu associato
+    dropLink.click(function() {
+
+        var actualMenu = $(this).next('.dropdown-menu');
+
+        // Reset
+        dropMenu.not(actualMenu).hide();
+        
+        // Apertura / chiusura dei menu
+        actualMenu.toggle();
+    });
+});
